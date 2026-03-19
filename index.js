7360d1270b98feaffbb24109c04e2a6b99ea1fb6
Derrick Owusu <owusuderek@gmail.com>
2026-03-18 21:41:35 -0700

    Vault secured

---
+++ index.js
@@ -0,0 +1,14 @@
+let code1;
+code1 = 7 + 3;
+
+let code2 = 90 - 100 / 2;
+
+let code3 = (2 % 2) + 39;
+
+let message = "The vault has been secured. The combination is: .";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
